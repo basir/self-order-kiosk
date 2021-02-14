bfd5ba57117c4d05d45747de481ee535a054c839
Basir <basir.jafarzadeh@gmail.com>
2021-02-14 09:34:05 +0330

    Video-02-Create-Home-Screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Card, CardActionArea, Typography } from '@material-ui/core';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import { useStyles } from '../styles';
+import Logo from '../components/Logo';
+
+export default function HomeScreen() {
+  const styles = useStyles();
+  return (
+    <Card>
+      <CardActionArea>
+        <Box className={[styles.root, styles.red]}>
+          <Box className={[styles.main, styles.center]}>
+            <Typography component="h6" variant="h6">
+              Fast & Easy
+            </Typography>
+            <Typography component="h1" variant="h1">
+              Order <br /> & pay <br /> here
+            </Typography>
+            <TouchAppIcon fontSize="large"></TouchAppIcon>
+          </Box>
+          <Box className={[styles.center, styles.green]}>
+            <Logo large></Logo>
+            <Typography component="h5" variant="h5">
+              Touch to start
+            </Typography>
+          </Box>
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+}
