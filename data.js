32cdaedcad33b10a791426cff8cc5fe4dbf6e9f2
Basir <basir.jafarzadeh@gmail.com>
2021-03-13 12:08:39 +0330

    Video-04-Create-Backend-Api-For-Categories

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = {
+  categories: [
+    {
+      name: 'Beverages',
+      image: '/images/beverages.jpg',
+    },
+    {
+      name: 'Breakfast',
+      image: '/images/breakfast.jpg',
+    },
+    {
+      name: 'Burgers',
+      image: '/images/burgers.jpg',
+    },
+  ],
+};
+
+module.exports = data;
